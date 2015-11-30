d9e38cb4c755f1ead8832fdcb30c06ca435c8cd3
Diego Russo <me@diegor.it>
2015-11-30 17:31:09 +0000

    Move mxms to another file

--- src/cannon/gendat.c
+++ src/cannon/gendat.c
@@ -5,7 +5,7 @@
 // This method generates matrix A and B. Matrices are just vector of doubles
 // split in blocks
 void gendat(int m, int l, int n, double *a, double *b) {
-    int     i, j, npes, N_DBLOCK, M_DBLOCK, L_DBLOCK;
+    int     i, j, nprocesses, N_DBLOCK, M_DBLOCK, L_DBLOCK;
     int     myrank, my2drank, mycoords[2];
     int     periods[2];
     int     dimensions[2];
@@ -15,7 +15,7 @@ void gendat(int m, int l, int n, double *a, double *b) {
 
     // Determines the size of the group associated with a communicator: it
     // returns number of processes in the group MPI_COMM_WORLD
-    MPI_Comm_size(MPI_COMM_WORLD, &npes);
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocesses);
 
     // Determines the rank of the calling process in the communicator: it
     // returns the rank of the calling process in the group MPI_COMM_WORLD
@@ -23,7 +23,7 @@ void gendat(int m, int l, int n, double *a, double *b) {
 
     // I have just two dimensions: they will be passed to build the MPI cart
     // communicator. Every dimension has the squre root of number of processors
-    dimensions[0] = dimensions[1] = (int)sqrt(npes);
+    dimensions[0] = dimensions[1] = (int)sqrt(nprocesses);
     // I set the periodic attribut to false for every dimension
     periods[0] = periods[1] = 0;
 
@@ -54,13 +54,14 @@ void gendat(int m, int l, int n, double *a, double *b) {
 
     // I split the matrices dimensions in blocks using the square root of the
     // number of processors I have available
-    M_DBLOCK = (int)(m / sqrt(npes));
-    L_DBLOCK = (int)(l / sqrt(npes));
-    N_DBLOCK = (int)(n / sqrt(npes));
+    M_DBLOCK = (int)(m / sqrt(nprocesses));
+    L_DBLOCK = (int)(l / sqrt(nprocesses));
+    N_DBLOCK = (int)(n / sqrt(nprocesses));
 
     debug_print("BLOCKS - M: %i L: %i N: %i \n", M_DBLOCK, L_DBLOCK, N_DBLOCK);
 
     // I generate data for matrix A (m x l) represented by an array
+    if (myrank == 0) printf("Generating data for matrix A[%d x %d]\n", m, l);
     for (j = 0; j < M_DBLOCK; j++) {
         for (i = 0; i < L_DBLOCK; i++) {
             a[j * L_DBLOCK + i] = (double)((mycoords[1] * L_DBLOCK) + i + 1);
@@ -69,6 +70,7 @@ void gendat(int m, int l, int n, double *a, double *b) {
     }
 
     // I generate data for matrix B (n x l) represented by an array
+    if (myrank == 0) printf("Generating data for matrix B[%d x %d]\n", l, n);
     for (i = 0; i < N_DBLOCK; i++) {
         for (j = 0; j < L_DBLOCK; j++) {
             // Multiplicative inverse
